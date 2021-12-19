d926aec15ba9c6c9d851120e9e85f60b0a925bbf
AlexZ005 <alexz005.info@gmail.com>
2021-12-19 15:33:47 +0200

    getting familiar with kubectl tests

--- k8s-deploy-services/02.kubectl.sh
+++ k8s-deploy-services/02.kubectl.sh
@@ -1,5 +1,2 @@
-#Getting familiar with kubectl commands.
-#Type some of those commands:
-#kubectl version
-#kubectl get pods
-#kubectl get nodes
+echo Getting familiar with kubectl commands.
+echo Type some of those commands: #kubectl version, #kubectl get pods, #kubectl get nodes
